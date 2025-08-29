789e7c459f9d8f89c3cf49c4a6c8cc56c228f57c
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2025-08-29 13:37:47 +0200

    add 'use_data_raw'

---
+++ inst/templates/data.R
@@ -0,0 +1,10 @@
+#' Name         : {{name}}
+#' Author       : {{author}}
+#' Date         : {{date}}
+#' Purpose      : description
+#' Files created: `data/processed/{{name}}.rds`
+#' Edits        :
+#'  - {{date}}: Created file.
+
+# Packages ----------------------------------------------------------------
+
