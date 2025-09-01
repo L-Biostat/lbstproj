b9ea2d8180c0892c52e616670cc6e257d3c6a345
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2025-09-01 10:10:51 +0200

    Add docs to create_project

--- R/create_project.R
+++ R/create_project.R
@@ -1,24 +1,73 @@
+#' Create a new project with standard structure
+#'
+#' Sets up a new project at the specified path, including a predefined
+#' directory structure and a `DESCRIPTION` file containing metadata such
+#' as title, client, department, author, and version.
+#'
+#' @details
+#' The following directory structure is created inside the project:
+#'
+#' ```
+#' ├── data
+#' │   ├── processed
+#' │   ├── raw
+#' │   └── tables
+#' ├── inst
+#' ├── docs
+#' ├── output
+#' │   ├── figures
+#' │   └── tables
+#' ├── R
+#' │   ├── data
+#' │   ├── figures
+#' │   └── tables
+#' └── report
+#' ```
+#'
+#' @param title Title of the project.
+#' @param client Name of the client.
+#' @param department Department code or name.
+#' @param author Author name (first and last).
+#' @param version Project version (default is `1.0`).
+#' @param path Path where the project should be created (default is current directory).
+#'
+#' @return Invisibly returns the active project path.
+#' @md
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' create_project(
+#'   title = "Example Project",
+#'   client = "Client Name",
+#'   department = "DEP",
+#'   author = "Jane Doe",
+#'   version = 0.1,
+#'   path = "." # uses current directory
+#' )
+#' }
 create_project <- function(
   title,
   client,
   department,
+  author,
   version = 1.0,
-  author
+  path = "."
 ) {
-  # Create project
-  usethis::ui_silence(
-    usethis::create_project(
-      path = ".",
-      rstudio = TRUE,
-      open = FALSE
-    )
+  # Check if the path exists
+  full_path <- fs::path_abs(path)
+  if (!fs::dir_exists(path)) {
+    cli::cli_abort("The specified path {.path {full_path}} does not exist.")
+  }
+  # Create project at specified path
+  usethis::ui_silence()
+  usethis::create_project(
+    path = path,
+    rstudio = TRUE,
+    open = FALSE
   )
-
-  cli::cli_alert_info(
-    "Setting active project to {.path {usethis::proj_get()}}"
-  )
-
-  rproj_file <- fs::dir_ls(".", type = "file", glob = "*.Rproj")
+  cli::cli_alert_info("Setting active project to {.path {full_path}}")
+  rproj_file <- fs::dir_ls(path, type = "file", glob = "*.Rproj")
   cli::cli_alert_info("Writing {.file {rproj_file}} ")
   cli::cli_alert_info("Creating project structure ")
 
@@ -27,6 +76,7 @@ create_project <- function(
     "data/processed",
     "data/raw",
     "data/tables",
+    "docs",
     "inst",
     "output/figures",
     "output/tables",
