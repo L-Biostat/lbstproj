b11db5fe4bb4a91bad615bd8d36ae94eda0f04f5
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2025-10-27 10:56:24 +0100

    modify file templates

--- inst/templates/function.R
+++ inst/templates/function.R
@@ -2,11 +2,15 @@
 #' Author       : {{author}}
 #' Date         : {{date}}
 #' Purpose      : description
-#' Files created: `output/functions/{{name}}.R`
+#' Files created:
 #' Edits        :
 #'  - {{date}}: Created file.
 #'
 #' !! WARNING !!
-#' All functions should be called as `package::function()` to avoid confusion about
-#' which package the function came from.
+#' All functions should be called as `package::function()` to avoid confusion
+#' about which package the function came from.
 
+# fmt: skip
+{{name}} <- function() {
+  
+}
