72e1f4a380d5d77229444609a399b084b01b59ba
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2025-11-27 15:09:26 +0100

    Add functions to load all functions files
    Along with new helper-defined tests

---
+++ R/load_functions.R
@@ -0,0 +1,46 @@
+#' Load functions from the `R/functions` directory
+#'
+#' These helper functions load one or all functions from the `R/functions`
+#' directory.
+#'
+#' @param name The name of the specific function file to load. It must be
+#'   relative to the `R/functions` directory and can include or omit the `.R`
+#'   extension.
+#'
+#' @examples
+#' \dontrun{
+#' load_all_functions()
+#' load_function("data_cleaning") # Works
+#' load_function("data_cleaning.R") # Also works
+#' load_function("R/functions/data_cleaning.R") # Fails
+#' }
+#'
+#' @name load_func
+NULL
+
+#' @describeIn load_func Load all functions files.
+#' @export
+load_all_functions <- function() {
+  fn_dir <- usethis::proj_path("R/functions")
+  # List all functions (*.R files) in `R/functions`
+  fn_files <- fs::dir_ls(fn_dir, glob = "*.R")
+  # Source each function file
+  purrr::walk(fn_files, source)
+}
+
+#' @describeIn load_func Load a single function file.
+#' @export
+load_function <- function(name) {
+  # Delete any extensions to the file name
+  clean_name <- gsub("\\.[Rr]$", "", name)
+  # Construct the full path to the function file
+  file_path <- usethis::proj_path("R/functions", clean_name, ext = "R")
+  # Ensure the file exists
+  if (!fs::file_exists(file_path)) {
+    cli::cli_abort(
+      "Function file {.path {fs::path_rel(file_path)}} does not exist."
+    )
+  }
+  # Source the specified function file
+  source(file_path)
+}
---
+++ tests/testthat/test-load_func.R
@@ -0,0 +1,45 @@
+test_that("All functions are loaded", {
+  tmp <- local_create_project()
+  # Create two functions
+  writeLines("a <- function() {}", fs::path(tmp, "R/functions/a.R"))
+  writeLines("b <- function() {}", fs::path(tmp, "R/functions/b.R"))
+  # Load all functions
+  load_all_functions()
+  # Check if they both exist in the environment
+  expect_true(exists("a", mode = "function"))
+  expect_true(exists("b", mode = "function"))
+})
+
+test_that("Only a single function is loaded", {
+  tmp <- local_create_project()
+  # Create two functions
+  writeLines("a <- function() {}", fs::path(tmp, "R/functions/a.R"))
+  writeLines("b <- function() {}", fs::path(tmp, "R/functions/b.R"))
+  # Load a single function and check existence
+  load_function("a")
+  expect_true(exists("a", mode = "function"))
+  # Load another function with extension and check existence
+  load_function("b.R")
+  expect_true(exists("b", mode = "function"))
+})
+
+test_that("Absolute file paths fail", {
+  tmp <- local_create_project()
+  # Create one functions
+  writeLines("a <- function() {}", fs::path(tmp, "R/functions/a.R"))
+  # Check if error when loading with absolute path
+  expect_error(
+    load_function(fs::path(tmp, "R/functions/a.R"))
+  )
+})
+
+test_that("Wrong file paths fail", {
+  tmp <- local_create_project()
+  # Create two functions
+  writeLines("a <- function() {}", fs::path(tmp, "R/functions/a.R"))
+  # Check if error when loading with absolute path
+  expect_error(
+    load_function("c.R"),
+    "does not exist"
+  )
+})
