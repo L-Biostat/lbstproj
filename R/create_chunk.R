158e1079f031495214ef4ea38dedc4cc70cdb989
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2026-02-16 14:23:11 +0100

    Update documentation

--- R/create_chunk.R
+++ R/create_chunk.R
@@ -1,14 +1,20 @@
-#' Standardized way to create a Rmd code chunk to insert a figure or a table
+#' Create a quarto code chunk from the TOT
 #'
-#' @param id The id of the figure as specified in the table of tables (TOT)
+#' Generate the quarto code chunk  to include a figure or table in a report
+#' based on the information in the table of tables (TOT). The TOT-element is
+#' identified from its ID. The chunk includes the correct path to the element as
+#' well as the caption and a quarto-safe chunk label (starting with `tbl` for
+#' tables and `fig` for figures to allow for cross-referencing).
+#'
+#' @param id The id of the element as specified in the table of tables (TOT)
 #' @param print Logical, whether to print the chunk to the console (default:
 #'   TRUE)
 #' @param copy Logical, whether to copy the chunk to the clipboard (default:
-#'   TRUE if in interactive mode)
+#'   TRUE if in interactive mode, FALSE otherwise)
 #' @param pad Logical, whether to pad the chunk with blank lines before and
 #'   after and add a page break after (default: FALSE)
 #'
-#' @return The rendered Rmd chunk as a character string, invisibly
+#' @return The rendered quarto chunk as a character string, invisibly
 create_chunk <- function(id, copy = TRUE, print = TRUE, pad = FALSE) {
   rlang::check_required(id)
   # Retrieve figure details from the TOT
