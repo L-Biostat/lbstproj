010eea582747c6b0b41f2fb585e2d91fd01c76f5
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2026-02-16 14:17:04 +0100

    Remove type-specific chunk-generating functions and adapt to quarto reporting

--- R/create_chunk.R
+++ R/create_chunk.R
@@ -1,32 +1,23 @@
 #' Standardized way to create a Rmd code chunk to insert a figure or a table
 #'
 #' @param id The id of the figure as specified in the table of tables (TOT)
-#' @param type The type of the entry, either "figure" or "table"
 #' @param print Logical, whether to print the chunk to the console (default:
 #'   TRUE)
 #' @param copy Logical, whether to copy the chunk to the clipboard (default:
 #'   TRUE if in interactive mode)
 #' @param pad Logical, whether to pad the chunk with blank lines before and
-#'   after and a pagebreak after (default: FALSE)
+#'   after and add a page break after (default: FALSE)
 #'
 #' @return The rendered Rmd chunk as a character string, invisibly
-create_chunk <- function(id, type, copy = TRUE, print = TRUE, pad = FALSE) {
+create_chunk <- function(id, copy = TRUE, print = TRUE, pad = FALSE) {
   rlang::check_required(id)
-  rlang::arg_match(type, values = c("figure", "table"))
   # Retrieve figure details from the TOT
   details <- get_info(id)
-  # Throw error if the corresponding entry is not a figure
-  if (details$type != type) {
-    cli::cli_abort(
-      "The entry with id {.val {id}} is not a {.val {type}} but a
-      {.val {details$type}}."
-    )
-  }
+  type <- details$type
   # Load the figure chunk template
-  chk_name <- paste0(type, "_chunk")
   template <- readLines(
     con = fs::path_package(
-      fs::path("templates", chk_name, ext = "Rmd"),
+      fs::path("templates", paste0(type, "_chunk"), ext = "qmd"),
       package = "lbstproj"
     )
   )
@@ -36,9 +27,8 @@ create_chunk <- function(id, type, copy = TRUE, print = TRUE, pad = FALSE) {
     fs::path("results/figures", details$name, ext = "png"),
     fs::path("data/tables", details$name, ext = "rds")
   )
-  prefix <- ifelse(type == "figure", "fig-", "tab-")
   template_data <- list(
-    id = paste0(prefix, details$name),
+    id = details$name,
     caption = details$caption,
     path = data_path
   )
@@ -46,7 +36,7 @@ create_chunk <- function(id, type, copy = TRUE, print = TRUE, pad = FALSE) {
   rendered_chunk <- whisker::whisker.render(template, template_data)
   # Pad the chunk if requested
   if (pad) {
-    rendered_chunk <- c("", rendered_chunk, "", "\\newpage", "")
+    rendered_chunk <- c("", rendered_chunk, "", "{{<pagebreak>}}", "")
   }
   # If needed, print the chunk to the console
   if (print) {
@@ -54,27 +44,9 @@ create_chunk <- function(id, type, copy = TRUE, print = TRUE, pad = FALSE) {
   }
   # Copy the chunk to the clipboard if in interactive mode
   if (rlang::is_interactive() && copy) {
-    clipr::write_clip(rendered_chunk, allow_non_interactive = FALSE)
+    writeClipboard(rendered_chunk)
     cli::cli_alert_success("The chunk has been copied to the clipboard.")
   }
   # Return the rendered chunk invisibly
   invisible(rendered_chunk)
-}
-
-#' Create a Rmd code chunk to insert a table
-#'
-#' @inheritParams create_chunk
-#'
-#' @export
-create_table_chunk <- function(id, copy = TRUE, print = TRUE, pad = FALSE) {
-  create_chunk(id = id, type = "table", copy = copy, print = print, pad = pad)
-}
-
-#' Create a Rmd code chunk to insert a figure
-#'
-#' @inheritParams create_chunk
-#'
-#' @export
-create_figure_chunk <- function(id, copy = TRUE, print = TRUE, pad = FALSE) {
-  create_chunk(id = id, type = "figure", copy = copy, print = print, pad = pad)
 }
