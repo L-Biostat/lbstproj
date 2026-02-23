5cfe58fcf1493aa4197814a369f7bb4f67c6d2f2
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2026-02-23 14:22:46 +0100

    Redefine utility function "ensure_dir_exists"

--- R/checks.R
+++ R/checks.R
@@ -1,4 +1,49 @@
-# Check directories and files ---------------------------------------------
+#' Ensure directory exists
+#'
+#' Checks if a directory exists and creates it if it doesn't. If the session is
+#' non-interactive, an error is thrown instead of prompting the user. If the
+#' directory already exists, the function silently ends.
+#'
+#' @param dir Character. The path of the directory to check/create.
+#' @param print Logical. If `TRUE`, prints a message to the CLI about the
+#'   creation status of the directory. Defaults to the global option `use.print`
+#'   (which defaults to `TRUE`).
+#'
+#' @return Invisibly returns the relative path of the directory if it exists or
+#'
+#' @keywords internal
+ensure_dir_exists <- function(dir, print = getOption("use.print", TRUE)) {
+  dir_rel_path <- proj_rel_path(dir)
+  if (fs::dir_exists(dir)) {
+    return(invisible(dir))
+  }
+  if (!interactive()) {
+    cli::cli_abort(
+      "Directory {dir_rel_path} does not exist and session is non-interactive."
+    )
+  }
+  ok <- usethis::ui_yeah(
+    x = "Directory {dir_rel_path} does not exist. Create it?",
+    yes = c("Yes"),
+    no = c("No"),
+    shuffle = FALSE
+  )
+  if (!isTRUE(ok)) {
+    if (isTRUE(print))
+      cli::cli_alert_warning("Aborted. Directory was not created.")
+    return(invisible(NULL))
+  }
+
+  fs::dir_create(dir, recurse = TRUE)
+
+  if (isTRUE(print)) {
+    cli::cli_alert_success(
+      "Created directory {dir_rel_path}."
+    )
+  }
+
+  invisible(dir_rel_path)
+}
 
 check_dir_exists <- function(dir) {
   # If dir does not exist, create it and warn user
