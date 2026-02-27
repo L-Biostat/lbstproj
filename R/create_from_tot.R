a423c35c13210cbaf8e6abbd0484a4dec718a5a1
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2026-02-27 11:06:10 +0100

    Create create_from_tot.R

---
+++ R/create_from_tot.R
@@ -0,0 +1,13 @@
+# -------------------------------------------------------------------------
+# ┌────────────┐
+# │  lbstproj  │
+# └────────────┘
+#
+# create_from_tot.R:
+# Create all files and directories from the table of contents (TOT) in the
+# project
+# -------------------------------------------------------------------------
+
+#' Create all scripts listed in the Table of Tables (TOT)
+#'
+#'
